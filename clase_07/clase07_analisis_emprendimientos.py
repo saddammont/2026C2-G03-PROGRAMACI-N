388e4ac3df358e7c39db212c93b5533408c64f0f
Saddam Montiel <saddammont@gmail.com>
2026-07-02 21:09:43 -0600

    final de clase

--- clase_07/clase07_analisis_emprendimientos.py
+++ clase_07/clase07_analisis_emprendimientos.py
@@ -6,3 +6,68 @@ sede usando listas, diccionarios, funciones, ciclos y condicionales.
 
 from sedes import sedes
 
+
+def calcular_total(lista_ventas):
+    """Recibo una lista, la sumo y retorno el total."""
+    return sum(lista_ventas)
+
+def calcular_promedio(lista_ventas):
+    """Retorna el promedio de las ventas de la lista ventas"""
+    return sum(lista_ventas) / len(lista_ventas)
+
+def calcular_porcentaje(total_ventas, meta): # total_ventas = total de ventas del emprendimeinto
+    """Calcula el procentaje de cumplimiento de la meta"""
+    return total_ventas / meta * 100
+
+def calcular_clasificacion(porcentaje_logro):
+    """"Clasifica el emprendimiento según porcentaje de cumplimiento de meta de ventas."""
+    if porcentaje_logro >= 100:
+        clasificacion_empredimiento = "Meta alcanzada, emprendimiento rentable"
+    elif porcentaje_logro >= 80:
+        clasificacion_empredimiento = "Observación, no se logró la meta."
+    else:
+        clasificacion_empredimiento = "ADVERTENCIA, problemas de rentabilidad. URGE ATENCIÓN."
+    return clasificacion_empredimiento
+
+def imprimir_reporte(reporte):
+    pass
+
+#print("Cantidad de sedes:", len(sedes))
+#print(type(sedes), "vrs", type(sedes[0]))
+#print("Datos por sede:", sedes[0].keys())
+#print("\nPrimer emprendimiento:", sedes[0]["nombre"])
+
+
+reporte = []
+provincias = set()
+
+
+
+for empredimiento in sedes:
+    #empredimiento = sedes[0]  # Extraigo el primer empredimeinto de la lista
+    ventas = empredimiento["ventas"]
+    meta = empredimiento["meta"]
+
+    total_emprendimiento = calcular_total(ventas)
+    promedio_emprendiemto = calcular_porcentaje(total_emprendimiento, meta)
+    promedio_diario = calcular_promedio(ventas)
+    clasificacion = calcular_clasificacion(promedio_emprendiemto)
+
+#    print("\nEmprendimiento:", empredimiento["nombre"])
+#    print("Total ventas:", total_emprendimiento)
+#    print("Porcentaje logro:", promedio_emprendiemto)
+#    print("Promedio diario:", promedio_diario)
+#    print("Análisis de emprendimiento:", clasificacion)
+    reporte.append(
+        {
+            "nombre": empredimiento["nombre"],
+            "provincia": empredimiento["provincia"],
+            "tipo": empredimiento["tipo"],
+            "total": total_emprendimiento,
+            "promedio": promedio_diario,
+            "porcentaje": promedio_emprendiemto,
+            "estado": clasificacion
+            
+        }
+    )
+    imprimir_reporte(reporte)
